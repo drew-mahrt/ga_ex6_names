3740bbfd8a0344aef2491f12b201a7629ae559f5
drew-mahrt <drew.mahrt@generalassemb.ly>
2016-01-03 05:29:41 -0500

    Added helper method

--- src/main/java/org/gradle/App.java
+++ src/main/java/org/gradle/App.java
@@ -1,23 +1,26 @@
-package org.gradle;
 
-/**
- * Created by kgalligan on 12/2/15.
- */
 public class App
 {
 	public static void main(String[] args)
 	{
 		// create a first method called cleanFullName that accepts two parameters: firstName, lastName
-		// this method should make all the letters of both those paramters lowercase and then capitalize the first letter of each
+		// Use the capitalize to make all the letters of both those paramters lowercase and then capitalize the first letter of each
 		// this method should return the combination of the first and last name as one string
 
 		// ask the user for their first name
 
 		// ask the user for their last name
 
-		// run the first method and store the results in a variable called fullName
+		// run the cleanFullName method and store the results in a variable called fullName
 
 		// print the fullName to the screen
+	}
+
+	private static String capitalize(String firstName)
+	{
+		firstName = firstName.toLowerCase();
+		firstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
+		return firstName;
 	}
 }
 
